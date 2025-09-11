ff5fbc35ac01485aefa2cb09bf6e01c94dc897b0
ayamir <lgt986452565@gmail.com>
2025-09-11 13:39:07 +0800

    feat: support bookmarks.nvim

---
+++ lua/base46/integrations/bookmarks.lua
@@ -0,0 +1,6 @@
+local colors = require("base46").get_theme_tb "base_30"
+
+return {
+  BookmarksNvimSign = { fg = colors.blue },
+  BookmarksNvimLine = { bold = true, italic = true, underline = true },
+}
